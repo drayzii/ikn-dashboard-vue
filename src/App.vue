fda0411d6174fffda49669633b60d4d06c54c346
Jonathan Shyaka <jonashyaka2@gmail.com>
2020-02-07 15:03:43 +0200

    Add Welcome page

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,17 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+  <div>
+    <b-navbar toggleable="lg" type="light" variant="light">
+      <b-navbar-brand class="logo">
+        <router-link to='/'>IKIRINGO</router-link>
+      </b-navbar-brand>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item class="menu-item">
+            <router-link to='/login'>Go to ikiringo.rw</router-link>
+          </b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
     <router-view/>
   </div>
 </template>
-
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
---
+++ src/views/Welcome.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="jumbotron-container">
+    <b-jumbotron
+      class="home-jumbotron"
+      header="Welcome to Ikiringo"
+      lead="Log in to continue"
+    >
+      <b-button class="login-button" variant="outline-primary">
+        <router-link to='/login'>Log In</router-link>
+      </b-button>
+    </b-jumbotron>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+};
+</script>
